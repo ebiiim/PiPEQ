3ecfb0ddb6fb48fface150cfb697f56d2321c535
ebiiim <mail@ebiiim.com>
2018-11-04 02:01:27 +0900

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")" || exit
 
 sudo apt install -y sox gnuplot
 
-sudo rm -f /usr/local/bin/recorder
+sudo rm -f /usr/local/bin/pipeq-recorder
 sudo rm -f /usr/local/bin/pipeq
-sudo ln -s "$(pwd)"/recorder /usr/local/bin
+sudo ln -s "$(pwd)"/pipeq-recorder /usr/local/bin
 sudo ln -s "$(pwd)"/pipeq /usr/local/bin
