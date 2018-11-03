fb42d51bf76c38bf3bd33d31e3ff2f655dad9dac
ebiiim <mail@ebiiim.com>
2018-11-04 01:44:50 +0900

    edit plot_combined

--- plot_combined.pl
+++ plot_combined.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-print STDERR "WARNING: No sanitization of the input is done at all. A malicious external user (e.g., via a web form) can easily compromise your system.\n";
+# Forked from Ulrich Klauer's plot_combined.pl https://sourceforge.net/p/sox/feature-requests/162/
 
 sub usage() {
 	print STDERR "Usage: $0 [sampling_rate] \"effect1 effargs1\" " .
@@ -23,13 +23,16 @@ my $desc = join(" ", @ARGV);
 
 print <<END;
 # gnuplot file
-set title 'SoX effects: $desc (rate=$rate)'
+set term pdfcairo enhanced size 8in, 3in
+# set title 'SoX effects: $desc (rate=$rate)'
 set xlabel 'Frequency (Hz)'
 set ylabel 'Amplitude Response (dB)'
 Fs=$rate
 o=2*pi/Fs
 set logscale x
 set grid xtics ytics
+set xtics (20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, "1k" 1000, "1k25" 1250, "1k6" 1600, "2k" 2000, "2k5" 2500, "3k15" 3150, "4k" 4000, "5k" 5000, "6k3" 6300, "8k" 8000, "10k" 10000, "12k5" 12500, "16k" 16000, "20k" 20000)
+set ytics -15, 3, 15
 set key off
 END
 
@@ -46,6 +49,6 @@ my $prod = join("*", map { "H_$_(f)" } (1..$l));
 print "H(f)=$prod\n";
 
 print <<END;
-plot [f=10:Fs/2] [-35:25] 20*log10(H(f))
-pause -1 'Hit return to continue'
+# plot [f=10:Fs/2] [-35:25] 20*log10(H(f))
+plot [f=20:20000] [-20:20] 20*log10(H(f))
 END
