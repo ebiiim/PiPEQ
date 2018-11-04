f9f837553f9082bc38d1cbb57b14659e16fd0952
ebiiim <mail@ebiiim.com>
2018-11-04 19:34:44 +0900

    fix sox options

--- pipeq.py
+++ pipeq.py
@@ -40,9 +40,9 @@ def _select_device():
 def build_command(input_device, rates, bits, buffers, sox_effects):
     """
     :param input_device: device index e.g. 2
-    :param rates: [input, internal, output] e.g. [48000, 96000, 48000]
-    :param bits: [input, internal, output] e.g. [16, 32, 16]
-    :param buffers: [input, eq, mix] e.g. [512, 512, 2048]
+    :param rates: [input, output] e.g. [48000, 48000]
+    :param bits: [input, output] e.g. [16, 16]
+    :param buffers: [input, sox] e.g. [512, 1024]
     :param sox_effects: [left, right] e.g. [['equalizer', '80', '5q', '-6'], ['equalizer', '10000', '2q', '3']]
     :return str: command
     """
@@ -56,20 +56,20 @@ def build_command(input_device, rates, bits, buffers, sox_effects):
     s2l = "sh -c " \
           "'sox --buffer "+str(buffers[1])+"" \
           " -t raw -b "+str(bits[0])+" -e signed -c 2 -r "+str(rates[0])+" $0" \
-          " -t raw -b "+str(bits[1])+" -e signed -c 1 -r "+str(rates[1])+" $1" \
+          " -t raw -b "+str(bits[0])+" -e signed -c 1 -r "+str(rates[0])+" $1" \
           " remix 1 "+' '.join(sox_effects[0])+"'" \
           " $L1 $L2"
     s2r = "sh -c " \
           "'sox --buffer "+str(buffers[1])+"" \
           " -t raw -b "+str(bits[0])+" -e signed -c 2 -r "+str(rates[0])+" $0" \
-          " -t raw -b "+str(bits[1])+" -e signed -c 1 -r "+str(rates[1])+" $1" \
+          " -t raw -b "+str(bits[0])+" -e signed -c 1 -r "+str(rates[0])+" $1" \
           " remix 2 "+' '.join(sox_effects[1])+"'" \
           " $R1 $R2"
     rec = "sh -c '"+' '.join(rec)+" | tee $0 > $1' $L1 $R1"
-    mix = "sox --buffer "+str(buffers[2])+" -M" \
-          " -t raw -b "+str(bits[1])+" -e signed -c 1 -r "+str(rates[1])+" $L2" \
-          " -t raw -b "+str(bits[1])+" -e signed -c 1 -r "+str(rates[1])+" $R2" \
-          " -t raw -b "+str(bits[2])+" -e signed -c 2 -r "+str(rates[2])+" -"
+    mix = "sox --buffer "+str(buffers[1])+" -M" \
+          " -t raw -b "+str(bits[0])+" -e signed -c 1 -r "+str(rates[0])+" $L2" \
+          " -t raw -b "+str(bits[0])+" -e signed -c 1 -r "+str(rates[0])+" $R2" \
+          " -t raw -b "+str(bits[1])+" -e signed -c 2 -r "+str(rates[1])+" -"
 
     return tmp + s2l + ' & ' + s2r + ' & ' + rec + ' & ' + mix
 
@@ -79,20 +79,17 @@ if __name__ == '__main__':
     show_devices()
     dev_in, dev_out = _select_device()
     sr_in = 48000
-    sr_proc = 192000
     sr_out = 48000
     bit_in = 16
-    bit_proc = 32
     bit_out = 16
-    buf_in = 256
-    buf_eq = 256
-    buf_mix = 2048
-    buf_out = 256
+    buf_in = 512
+    buf_sox = 1024
+    buf_out = 512
     eq_l = ['gain', '-3']
     eq_r = ['gain', '-3']
     if len(sys.argv) == 3:
         eq_l += build_eq(parse_roomeq(sys.argv[1]))
         eq_r += build_eq(parse_roomeq(sys.argv[2]))
-    cmd = build_command(dev_in, [sr_in, sr_proc, sr_out], [bit_in, bit_proc, bit_out], [buf_in, buf_eq, buf_mix], [eq_l, eq_r])
+    cmd = build_command(dev_in, [sr_in, sr_out], [bit_in, bit_out], [buf_in, buf_sox], [eq_l, eq_r])
     print(cmd)
     play_stdin(cmd, sr_out, bit_out, buf_out, dev_out)
