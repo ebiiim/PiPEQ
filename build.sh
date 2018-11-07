cf6c1f0abd0ea707f4154f348dce69af70101ba0
ebiiim <mail@ebiiim.com>
2018-11-08 01:09:26 +0900

    show_eq

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ cd "$(dirname "$0")" || exit
 pyinstaller --onefile pipeq.py
 pyinstaller --onefile recorder.py
 pyinstaller --onefile get_devices.py
+pyinstaller --onefile plot_eq.py
 mv dist/recorder dist/pipeq-recorder
 mv dist/get_devices dist/pipeq-get-devices
+mv dist/plot_eq dist/pipeq-show-eq
 
+cp plot_combined.pl dist/pipeq-plot-eq
 cp install.sh dist/install.sh
 cp config.txt dist/config.txt
--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sudo apt install -y sox gnuplot
 sudo rm -f /usr/local/bin/pipeq
 sudo rm -f /usr/local/bin/pipeq-recorder
 sudo rm -f /usr/local/bin/pipeq-get-devices
+sudo rm -f /usr/local/bin/pipeq-show-eq
+sudo rm -f /usr/local/bin/pipeq-plot-eq
 sudo ln -s "$(pwd)"/pipeq /usr/local/bin
 sudo ln -s "$(pwd)"/pipeq-recorder /usr/local/bin
 sudo ln -s "$(pwd)"/pipeq-get-devices /usr/local/bin
+sudo ln -s "$(pwd)"/pipeq-show-eq /usr/local/bin
+sudo ln -s "$(pwd)"/pipeq-plot-eq /usr/local/bin
