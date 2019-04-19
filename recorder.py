3857a175cd6965388ffb6d497122b5faa1351ff8
ebiiim <mail@ebiiim.com>
2019-04-19 23:12:22 +0900

    update: improve stability

--- recorder.py
+++ recorder.py
@@ -2,29 +2,35 @@ import pyaudio
 
 
 def write_input_to_stdout(input_device_index, sample_rate, sample_bit, buffer_size):
-    pa = pyaudio.PyAudio()
-    bit_fmt = pyaudio.paInt16
-    if sample_bit == 24:
-        bit_fmt = pyaudio.paInt24
-    if sample_bit == 32:
-        bit_fmt = pyaudio.paInt32
-    stream = pa.open(format=bit_fmt,
-                     channels=2,
-                     rate=sample_rate,
-                     frames_per_buffer=buffer_size,
-                     input=True,
-                     input_device_index=input_device_index,
-                     output=False,
-                     )
-    stream.start_stream()
-    while stream.is_active():
-        buffer = stream.read(buffer_size)
-        if len(buffer) == 0:
-            break
-        sys.stdout.buffer.write(buffer)
-    stream.stop_stream()
-    stream.close()
-    pa.terminate()
+    while True:
+        pa = pyaudio.PyAudio()
+        bit_fmt = pyaudio.paInt16
+        if sample_bit == 24:
+            bit_fmt = pyaudio.paInt24
+        if sample_bit == 32:
+            bit_fmt = pyaudio.paInt32
+        stream = pa.open(format=bit_fmt,
+                         channels=2,
+                         rate=sample_rate,
+                         frames_per_buffer=buffer_size,
+                         input=True,
+                         input_device_index=input_device_index,
+                         output=False,
+                         )
+        stream.start_stream()
+        try:
+            while stream.is_active():
+                try:
+                    buffer = stream.read(buffer_size)
+                except OSError as e:
+                    sys.stderr.write(f'Error: {e}')
+                    buffer = b'\x00' * buffer_size
+                sys.stdout.buffer.write(buffer)
+            stream.stop_stream()
+            stream.close()
+            pa.terminate()
+        except OSError as e:
+            sys.stderr.write(f'Error: {e}')
 
 
 if __name__ == '__main__':
